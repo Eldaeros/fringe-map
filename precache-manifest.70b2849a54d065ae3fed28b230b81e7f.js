01f02efe40e299a536d15fa4bddb144525e1eb9c
Peter Gates <peter@Peters-MBP.gateway>
2019-08-24 12:39:12 +1000

    Updates

---
+++ precache-manifest.70b2849a54d065ae3fed28b230b81e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50707ad64fe0b0d988d11f897914b174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7697694f0eb88b5a76b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73354fb24b916aaa99c3",
+    "url": "/static/js/2.aa203a7d.chunk.js"
+  },
+  {
+    "revision": "e7697694f0eb88b5a76b",
+    "url": "/static/js/main.097f02a5.chunk.js"
+  },
+  {
+    "revision": "fe2cae0e57947e046ae6",
+    "url": "/static/js/runtime~main.f7d4aae4.js"
+  }
+]);
