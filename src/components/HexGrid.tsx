3bcba0292eeea85cf92b586c2986392b8b43e4aa
Peter Gates <peter@Peters-MBP.gateway>
2019-08-24 14:35:19 +1000

    Updated hex numbering style

--- src/components/HexGrid.tsx
+++ src/components/HexGrid.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent, Component } from "react";
 import styled from "styled-components";
-import SvgHexagon from './svg/Hexagon';
-import { render } from "react-dom";
+import SvgHexagon from "./svg/Hexagon";
+
+const _ = require("lodash");
 
 interface HexGridProps {
     size: {
@@ -15,34 +16,45 @@ const COLUMN_OFFSET = 1.48;
 const ROW_OFFSET = 0.43;
 const ODD_OFFSET = 0.74;
 
-export const HexGrid: FunctionComponent<HexGridProps> = (props: HexGridProps) => {
+export const HexGrid: FunctionComponent<HexGridProps> = (
+    props: HexGridProps,
+) => {
     const gridLayout: JSX.Element[] = [];
     const rows = props.size.height * 2;
     const columns = props.size.width / 2;
     // Calculate Rows
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         for (let colIndex = 0; colIndex < columns; colIndex++) {
-
-            const xGridCoord = (rowIndex % 2 ? colIndex * 2 +2 : colIndex * 2 + 1);
-            const yGridCoord = Math.floor(rowIndex / 2) + 1;
+            const xGridCoord = _.padStart(
+                rowIndex % 2 ? colIndex * 2 + 2 : colIndex * 2 + 1,
+                2,
+                "0",
+            );
+            const yGridCoord = _.padStart(Math.floor(rowIndex / 2) + 1, 2, "0");
 
             // Odd Rows are offset by 75%
-            const rowOffset =  rowIndex % 2 ? ODD_OFFSET * HEX_SIZE : 0;
+            const rowOffset = rowIndex % 2 ? ODD_OFFSET * HEX_SIZE : 0;
             gridLayout.push(
                 <Hexagon
                     key={`hex-${yGridCoord}-${xGridCoord}`}
                     data-hex-text={`hex-${yGridCoord}-${xGridCoord}`}
-                    hexText={`${yGridCoord},${xGridCoord}`}
+                    hexText={`${yGridCoord}${xGridCoord}`}
                     size={HEX_SIZE}
-                    position={[colIndex * HEX_SIZE * COLUMN_OFFSET + rowOffset, rowIndex * HEX_SIZE * ROW_OFFSET]}
+                    position={[
+                        colIndex * HEX_SIZE * COLUMN_OFFSET + rowOffset,
+                        rowIndex * HEX_SIZE * ROW_OFFSET,
+                    ]}
                 />,
             );
         }
     }
 
-    return (<>{gridLayout}</>);
+    return (
+        <>
+            {gridLayout}
+        </>
+    );
 };
-
 
 interface HexagonProps {
     size: number;
@@ -55,14 +67,13 @@ interface HexagonState {
 }
 
 export class Hexagon extends Component<HexagonProps, HexagonState> {
-
     state: HexagonState = {
-        hover: false
-    }
+        hover: false,
+    };
 
     _toggleHover = () => {
-        this.setState({hover: !this.state.hover})
-    }
+        this.setState({ hover: !this.state.hover });
+    };
 
     render() {
         return (
@@ -72,9 +83,7 @@ export class Hexagon extends Component<HexagonProps, HexagonState> {
                 onMouseLeave={this._toggleHover}
                 hover={this.state.hover}
             >
-            <HexLabel>
-                {this.props.hexText}
-            </HexLabel>
+                <HexLabel size={this.props.size}>{this.props.hexText}</HexLabel>
                 <SvgHexagon
                     width={this.props.size}
                     height={this.props.size}
@@ -83,7 +92,7 @@ export class Hexagon extends Component<HexagonProps, HexagonState> {
                     strokeWidth={this.state.hover ? 20 : 10}
                 />
             </HexagonContainer>
-        )
+        );
     }
 }
 
@@ -95,21 +104,29 @@ const HexagonContainer = styled.div`
     width: ${(props: HexagonContainerProps) => props.size}px;
     height: ${(props: HexagonContainerProps) => props.size}px;
     transform: translate(
-        ${(props: HexagonContainerProps) => `${props.position[0]}px,${props.position[1]}px`}
+        ${(props: HexagonContainerProps) =>
+            `${props.position[0]}px,${props.position[1]}px`}
     );
-    z-index: ${(props: HexagonContainerProps) => props.hover ? `0` : `1`};
+    z-index: ${(props: HexagonContainerProps) => (props.hover ? `0` : `1`)};
 
     > div {
-        // font-weight: ${(props: HexagonContainerProps) => props.hover ? `600` : `normal`}
-        color: ${(props: HexagonContainerProps) => props.hover ? `#b5b5b5` : ``}
+        // font-weight: ${(props: HexagonContainerProps) =>
+            props.hover ? `600` : `normal`}
+        color: ${(props: HexagonContainerProps) =>
+            props.hover ? `#b5b5b5` : ``}
     }
 
-`
+`;
+
+interface HexLabelProps {
+    size: number;
+}
 const HexLabel = styled.div`
     position: relative;
-    top: 25px;
+    top: ${(props: HexLabelProps) => props.size * 0.25}px;
     text-align: center;
     color: #626262;
-    font-family: roboto-condensed, sans-serif; 
+    font-family: roboto-mono, sans-serif;
     font-size: 12px;
-`
+    letter-spacing: -0.05em;
+`;
