31740d5de92e8d595795f44b321852e802905da5
Peter Gates <peter@Peters-MBP.gateway>
2019-08-25 14:17:41 +1000

    Add types for scss files

---
+++ src/css/colors.scss.d.ts
@@ -0,0 +1,6 @@
+export const backgroundColor: string;
+export const fontColor: string;
+export const hexFill: string;
+export const hexStroke: string;
+export const hexFillHover: string;
+export const hexStrokeHover: string;
---
+++ src/css/dimensions.scss.d.ts
@@ -0,0 +1,2 @@
+export const hexStrokeWidth: string;
+export const hexStrokeWidthHover: string;
